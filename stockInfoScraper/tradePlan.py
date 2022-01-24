b90c4d17b889060954cd4c4f9cfdad6296451c7f
Jamison Chen <jamisonforeverandever@gmail.com>
2022-01-24 22:38:49 +0800

    deploy

--- stockInfoScraper/tradePlan.py
+++ stockInfoScraper/tradePlan.py
@@ -1,37 +1,41 @@
 from .models import TradePlan, TradeRecord
 from django.db.models import Sum
 
+
 class TradePlanView:
     def __init__(self):
         pass
 
     def createPlan(self, sid, planType, targetPrice, targetQuantity):
-        plan = TradePlan(sid=sid, planType=planType, targetPrice=targetPrice,
-                         targetQuantity=targetQuantity)
+        plan = TradePlan(
+            sid=sid,
+            planType=planType,
+            targetPrice=targetPrice,
+            targetQuantity=targetQuantity,
+        )
         plan.save()
 
     def readPlan(self, sidList):
         dictResultList = []
         if sidList == []:
-            autoSidQuery = TradeRecord.objects.values('sid').annotate(sum=Sum('dealQuantity')).filter(sum__gt=0).values('sid')
-            for each in autoSidQuery:
-                sidList.append(each["sid"])
-        q = TradePlan.objects.filter(sid__in=sidList)
+            q = TradePlan.objects.all()
+        else:
+            q = TradePlan.objects.filter(sid__in=sidList)
         for each in q:
-            dictResultList.append({
-                "id": each.id,
-                "sid": each.sid,
-                "plan-type": each.planType,
-                "target-price": each.targetPrice,
-                "target-quantity": each.targetQuantity
-            })
+            dictResultList.append(
+                {
+                    "id": each.id,
+                    "sid": each.sid,
+                    "plan-type": each.planType,
+                    "target-price": each.targetPrice,
+                    "target-quantity": each.targetQuantity,
+                }
+            )
         return dictResultList
 
     def updatePlan(self, ID, planType, targetPrice, targetQuantity):
         TradePlan.objects.filter(id=ID).update(
-            planType = planType,
-            targetPrice = targetPrice,
-            targetQuantity = targetQuantity
+            planType=planType, targetPrice=targetPrice, targetQuantity=targetQuantity
         )
 
     def deletePlan(self, ID):
