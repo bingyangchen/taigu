a6e3e905cfa56db34069eed09cb457c983522a24
Jamison Chen <jamisonforeverandever@gmail.com>
2022-02-21 21:28:47 +0800

    update

--- investment/production_settings.py
+++ investment/production_settings.py
@@ -1,11 +1,12 @@
 import dj_database_url
 from .settings import *
+import os
 
 # heroku使用的資料庫為PostgreSQL，所以要修改資料庫設定
 DATABASES = {
-    'default': dj_database_url.config(),
+    "default": dj_database_url.config(),
 }
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+ALLOWED_HOSTS = ["*"]
 DEBUG = False
