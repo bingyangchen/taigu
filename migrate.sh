a4e5b2773b8ec778dcebd39be1c6489fffba598a
Jamison Chen <106208004@g.nccu.edu.tw>
2022-04-30 13:03:16 +0800

    add deploy script

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-# run this script after the deploying process is completed on Heroku
+# Only run this script after the deploying process is completed on Heroku.
 heroku run python manage.py migrate -a investment-backend
