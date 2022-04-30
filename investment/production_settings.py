59e8da800286232bed4f878a935edaa9852f5c5f
Jamison Chen <106208004@g.nccu.edu.tw>
2022-04-30 22:02:50 +0800

    install dotenv, move important info into .env

--- investment/production_settings.py
+++ investment/production_settings.py
@@ -1,12 +1,8 @@
 import dj_database_url
 from .settings import *
-import os
 
-# heroku使用的資料庫為PostgreSQL，所以要修改資料庫設定
 DATABASES = {
     "default": dj_database_url.config(),
 }
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-ALLOWED_HOSTS = ["*"]
 DEBUG = False
