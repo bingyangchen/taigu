e33105fe002b2e465cfb10b636df92925a075fa1
Jamison Chen <106208004@g.nccu.edu.tw>
2022-04-30 22:08:37 +0800

    update production_settings.py

--- investment/production_settings.py
+++ investment/production_settings.py
@@ -1,8 +1,12 @@
 import dj_database_url
+import os
+
 from .settings import *
 
 DATABASES = {
     "default": dj_database_url.config(),
 }
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+ALLOWED_HOSTS = ["*"]
 DEBUG = False
