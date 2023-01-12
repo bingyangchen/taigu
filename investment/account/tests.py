ea540c051d0c273c52c0b3c8c20195a32fe495ca
Jamison Chen <106208004@g.nccu.edu.tw>
2023-01-12 10:22:17 +0800

    Add test files

--- investment/account/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-# Run the following command:
-# `python manage.py test investment.account.tests`
-print("" == None)
-print("123" == None)
-print(0 == None)
-print(1 == None)
-print(None == None)
--- investment/account/views.py
+++ investment/account/views.py
@@ -2,7 +2,7 @@ import json
 
 from django.http import JsonResponse, HttpRequest, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import check_password
 
@@ -76,7 +76,7 @@ def check_login(request: HttpRequest):
 
 
 @csrf_exempt
-@require_POST
+@require_GET
 @require_login
 def logout(request: HttpRequest):
     res = {"success": False}
