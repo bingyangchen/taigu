8454642ae9ea4831f851336bb6ddc02ed0ff74fd
Jamison Chen <106208004@g.nccu.edu.tw>
2023-02-15 17:50:11 +0800

    Update wsgi settings

--- investment/wsgi.py
+++ investment/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling  # for Heroku
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "investment.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "investment.production_settings")
 
 # application = get_wsgi_application()
 application = Cling(get_wsgi_application())  # for Heroku
