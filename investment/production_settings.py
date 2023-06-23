9dfefffa0941ba16675dab28bf311476d0c72c3f
Jamison-Chen <106208004@g.nccu.edu.tw>
2023-06-23 17:35:33 +0800

    update allowed hosts for production

--- investment/production_settings.py
+++ investment/production_settings.py
@@ -6,7 +6,7 @@ DATABASES = {"default": dj_database_url.config()}
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "trade-smartly.github.io"]
+ALLOWED_HOSTS = ["trade-smartly-backend-51d59e0a00fc.herokuapp.com"]
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https?://(localhost|127\.0\.0\.1):300[0-9]$",
     r"^https://trade-smartly\.github\.io$",
