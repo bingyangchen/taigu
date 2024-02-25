b8d48de55b69473a3754b552bad390847eeeb408
Jamison-Chen <106208004@g.nccu.edu.tw>
2024-02-25 13:26:43 +0800

    add aws ec2 public dns into ALLOWED_HOST of production_setting

--- main/production_settings.py
+++ main/production_settings.py
@@ -3,13 +3,13 @@ import dj_database_url
 from .settings import *  # noqa: F403
 
 DATABASES = {"default": dj_database_url.config()}
-
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-ALLOWED_HOSTS = ["trade-smartly-backend-51d59e0a00fc.herokuapp.com"]
+ALLOWED_HOSTS = [
+    "trade-smartly-backend-51d59e0a00fc.herokuapp.com",  # Heroku
+    "ec2-18-141-180-28.ap-southeast-1.compute.amazonaws.com",  # AWS EC2
+]
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https?://(localhost|127\.0\.0\.1):300[0-9]$",
     r"^https://trade-smartly\.github\.io$",
 ]
-
 DEBUG = False
