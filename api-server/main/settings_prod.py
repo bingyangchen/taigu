6fa2d18997b594240e487976f2adeaaa27005366
Bing-Yang Chen <bryan.chen.429@gmail.com>
2025-04-05 12:03:26 +0800

    Remove unnecessary CORS_ALLOWED_ORIGIN_REGEXES from production and development settings

--- api-server/main/settings_prod.py
+++ api-server/main/settings_prod.py
@@ -2,7 +2,3 @@ from .settings import *  # noqa: F403
 
 DEBUG = False
 ALLOWED_HOSTS = ["trade-smartly.com"]
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https?://(localhost|127\.0\.0\.1):300[0-9]$",
-    r"^https://trade-smartly\.com$",
-]
