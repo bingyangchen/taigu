0224f4baee3912abc0ffd6dde72b2512f80dfcc0
Bing-Yang Chen <bryan.chen.429@gmail.com>
2025-04-06 12:13:49 +0000

    Refactor ErrorList component styles and structure
    - Update ErrorList.module.scss to reposition error messages at the bottom left with a new animation.
    - Modify ErrorList.tsx to reflect the new structure, including changes to class names and layout for better responsiveness.
    - Enhance the close button functionality and styling for improved user experience.

--- frontend/src/components/ErrorList/ErrorList.tsx
+++ frontend/src/components/ErrorList/ErrorList.tsx
@@ -10,49 +10,50 @@ import { removeError } from "../../redux/slices/ErrorSlice";
 import type { AppDispatch, RootState } from "../../redux/store";
 
 function mapStateToProps(rootState: RootState) {
-    const { errors } = rootState.error;
-    return { errors };
+  const { errors } = rootState.error;
+  return { errors };
 }
 
 interface Props extends ReturnType<typeof mapStateToProps> {
-    dispatch: AppDispatch;
+  dispatch: AppDispatch;
 }
 
 interface State {}
 
 class ErrorList extends React.Component<Props, State> {
-    public state: State;
-    private errorRoot: HTMLElement;
-    public constructor(props: Props) {
-        super(props);
-        this.state = {};
-        this.errorRoot = document.getElementById("error-root")!;
-    }
-    public componentDidMount(): void {}
-    public render(): React.ReactNode {
-        return ReactDOM.createPortal(
-            <div className={styles.main}>
-                {this.props.errors.map((each, idx) => {
-                    return (
-                        <div key={idx} className={styles.box}>
-                            {each.message}
-                            <RoundButton
-                                onClick={() =>
-                                    this.handleClickRemoveMessage(idx)
-                                }
-                            >
-                                <IconXLarge sideLength="10" color="#FFF" />
-                            </RoundButton>
-                        </div>
-                    );
-                })}
-            </div>,
-            this.errorRoot
-        );
-    }
-    private handleClickRemoveMessage = (idx: number): void => {
-        this.props.dispatch(removeError(idx));
-    };
+  public state: State;
+  private errorRoot: HTMLElement;
+  public constructor(props: Props) {
+    super(props);
+    this.state = {};
+    this.errorRoot = document.getElementById("error-root")!;
+  }
+  public componentDidMount(): void {}
+  public render(): React.ReactNode {
+    return ReactDOM.createPortal(
+      <div className={styles.main}>
+        {this.props.errors.map((each, idx) => {
+          return (
+            <div key={idx} className={styles.toast_outer}>
+              <div className={styles.toast}>
+                {each.message}
+                <RoundButton
+                  onClick={() => this.handleClickRemoveMessage(idx)}
+                  className={styles.close_button}
+                >
+                  <IconXLarge sideLength="8" color="#FFF" />
+                </RoundButton>
+              </div>
+            </div>
+          );
+        })}
+      </div>,
+      this.errorRoot
+    );
+  }
+  private handleClickRemoveMessage = (idx: number): void => {
+    this.props.dispatch(removeError(idx));
+  };
 }
 
 export default connect(mapStateToProps)(ErrorList);
