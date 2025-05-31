f10a39d7e1fb3662c0030d32e5d284589994926e
Bing-Yang Chen <bryan.chen.429@gmail.com>
2025-05-31 20:08:22 +0800

    Enhance Footer and Details components with improved styles and structure (#44)

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -8,47 +8,45 @@ import type { Subpage } from "../../types";
 import Env from "../../utils/env";
 
 interface Props extends IRouter {
-    subpages: Subpage[];
+  subpages: Subpage[];
 }
 
 interface State {}
 
 class Footer extends React.Component<Props, State> {
-    public state: State;
-    public constructor(props: Props) {
-        super(props);
-        this.state = {};
-    }
-    public render(): React.ReactNode {
-        return (
-            <div className={styles.main}>
-                {this.props.subpages.map((subpage, idx) => {
-                    return (this.props.router.location.pathname.indexOf(
-                        subpage.path
-                    ) === 0 &&
-                        subpage.path !== Env.frontendRootPath) ||
-                        (this.props.router.location.pathname === subpage.path &&
-                            subpage.path === Env.frontendRootPath) ? (
-                        <div // prevent auto navigate one more time
-                            key={idx}
-                            className={`${styles.icon_outer} ${styles.active}`}
-                        >
-                            {subpage.icon}
-                        </div>
-                    ) : (
-                        <NavLink
-                            to={subpage.path}
-                            key={idx}
-                            end={subpage.path === Env.frontendRootPath}
-                            className={styles.icon_outer}
-                        >
-                            {subpage.icon}
-                        </NavLink>
-                    );
-                })}
+  public state: State;
+  public constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+  public render(): React.ReactNode {
+    return (
+      <div className={styles.main}>
+        {this.props.subpages.map((subpage, idx) => {
+          return (this.props.router.location.pathname.indexOf(subpage.path) === 0 &&
+            subpage.path !== Env.frontendRootPath) ||
+            (this.props.router.location.pathname === subpage.path &&
+              subpage.path === Env.frontendRootPath) ? (
+            <div // prevent auto navigate one more time
+              key={idx}
+              className={`${styles.icon_outer} ${styles.active}`}
+            >
+              <div className={styles.icon_inner}>{subpage.icon}</div>
             </div>
-        );
-    }
+          ) : (
+            <NavLink
+              to={subpage.path}
+              key={idx}
+              end={subpage.path === Env.frontendRootPath}
+              className={styles.icon_outer}
+            >
+              <div className={styles.icon_inner}>{subpage.icon}</div>
+            </NavLink>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default withRouter(Footer);
