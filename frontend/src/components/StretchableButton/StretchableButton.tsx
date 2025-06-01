feb35ea5214fc00aa053e251e360c6a45f42551f
Bing-Yang Chen <bryan.chen.429@gmail.com>
2025-06-01 09:37:38 +0800

    Refactor StretchableButton and add IconBrandStrategy
    - Updated the StretchableButton component to include a new icon, IconBrandStrategy, replacing IconLightbulb in various instances.
    - Adjusted the FullLogo styles to modify the font size for better visual consistency.

--- frontend/src/components/StretchableButton/StretchableButton.tsx
+++ frontend/src/components/StretchableButton/StretchableButton.tsx
@@ -4,184 +4,172 @@ import styles from "./StretchableButton.module.scss";
 import React, { MouseEvent } from "react";
 
 import {
-    CashDividendRecordModal,
-    Modal,
-    StockSearchModal,
-    TradePlanModal,
-    TradeRecordModal,
+  CashDividendRecordModal,
+  Modal,
+  StockSearchModal,
+  TradePlanModal,
+  TradeRecordModal,
 } from "..";
 import {
-    IconCoin,
-    IconLightbulb,
-    IconMagnifier,
-    IconPencilSquare,
+  IconBrandStrategy,
+  IconCoin,
+  IconMagnifier,
+  IconPencilSquare,
 } from "../../icons";
 import { IRouter, withRouter } from "../../router";
 import Util from "../../utils/util";
 
 interface Props extends IRouter {
-    defaultSid?: string;
+  defaultSid?: string;
 }
 
 interface State {
-    isActive: boolean;
-    activeModalName:
-        | "createTradeRecord"
-        | "createCashDividendRecord"
-        | "createTradePlan"
-        | "search"
-        | null;
+  isActive: boolean;
+  activeModalName:
+    | "createTradeRecord"
+    | "createCashDividendRecord"
+    | "createTradePlan"
+    | "search"
+    | null;
 }
 
 class StretchableButton extends React.Component<Props, State> {
-    public state: State;
-    public constructor(props: Props) {
-        super(props);
-        this.state = {
-            isActive: false,
-            activeModalName: null,
-        };
-    }
-    public componentDidMount(): void {}
-    public componentDidUpdate(
-        prevProps: Readonly<Props>,
-        prevState: Readonly<State>,
-        snapshot?: any
-    ): void {
-        if (
-            prevProps.router.location.hash === "#@" &&
-            this.props.router.location.hash === ""
-        ) {
-            this.hide();
-        }
-    }
-    public componentWillUnmount(): void {}
-    public render(): React.ReactNode {
-        return (
-            <>
-                {this.activeModal}
-                <div
-                    className={`${styles.main} ${
-                        this.state.isActive ? styles.active : ""
-                    }`}
-                    onClick={this.toggle}
-                >
-                    <img className={styles.button_image} src={logo} alt="" />
-                    <div className={styles.button_container}>
-                        <div
-                            className={styles.button}
-                            onClick={(e: MouseEvent) => {
-                                e.stopPropagation();
-                                this.hide();
-                                this.setState({
-                                    activeModalName: "createTradeRecord",
-                                });
-                            }}
-                        >
-                            <IconPencilSquare sideLength="20" />
-                            <div className={styles.hint_text}>交易紀錄</div>
-                        </div>
-                        <div
-                            className={styles.button}
-                            onClick={(e: MouseEvent) => {
-                                e.stopPropagation();
-                                this.hide();
-                                this.setState({
-                                    activeModalName: "createCashDividendRecord",
-                                });
-                            }}
-                        >
-                            <IconCoin sideLength="20" />
-                            <div className={styles.hint_text}>現金股利</div>
-                        </div>
-                        <div
-                            className={styles.button}
-                            onClick={(e: MouseEvent) => {
-                                e.stopPropagation();
-                                this.hide();
-                                this.setState({
-                                    activeModalName: "createTradePlan",
-                                });
-                            }}
-                        >
-                            <IconLightbulb sideLength="23" />
-                            <div className={styles.hint_text}>買賣計畫</div>
-                        </div>
-                        <div
-                            className={styles.button}
-                            onClick={(e: MouseEvent) => {
-                                e.stopPropagation();
-                                this.hide();
-                                this.setState({ activeModalName: "search" });
-                            }}
-                        >
-                            <IconMagnifier sideLength="20" />
-                            <div className={styles.hint_text}>搜尋</div>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.background} onClick={this.hide} />
-            </>
-        );
-    }
-    private get activeModal(): React.ReactElement<typeof Modal> | null {
-        if (this.state.activeModalName === "createTradeRecord") {
-            return (
-                <TradeRecordModal
-                    defaultSid={this.props.defaultSid}
-                    hideModal={Util.getHideModalCallback(this)}
-                />
-            );
-        } else if (this.state.activeModalName === "createCashDividendRecord") {
-            return (
-                <CashDividendRecordModal
-                    defaultSid={this.props.defaultSid}
-                    hideModal={Util.getHideModalCallback(this)}
-                />
-            );
-        } else if (this.state.activeModalName === "createTradePlan") {
-            return (
-                <TradePlanModal
-                    defaultSid={this.props.defaultSid}
-                    hideModal={Util.getHideModalCallback(this)}
-                />
-            );
-        } else if (this.state.activeModalName === "search") {
-            return (
-                <StockSearchModal hideModal={Util.getHideModalCallback(this)} />
-            );
-        }
-        return null;
-    }
-    private toggle = (e: MouseEvent): void => {
-        e.stopPropagation();
-        this.setState((state, props) => {
-            if (state.isActive) {
-                setTimeout(() => {
-                    if (this.props.router.location.hash === "#@") {
-                        this.props.router.navigate(-1);
-                    }
-                    Util.changePWAThemeColor("#fff");
-                    document.body.style.overscrollBehaviorY = "initial";
-                });
-            } else {
-                document.body.style.overscrollBehaviorY = "contain";
-                setTimeout(() => {
-                    this.props.router.navigate("#@");
-                    Util.changePWAThemeColor("#888");
-                });
-            }
-            return { isActive: !state.isActive };
-        });
+  public state: State;
+  public constructor(props: Props) {
+    super(props);
+    this.state = {
+      isActive: false,
+      activeModalName: null,
     };
-    private hide = (): void => {
-        if (this.props.router.location.hash === "#@") {
+  }
+  public componentDidMount(): void {}
+  public componentDidUpdate(
+    prevProps: Readonly<Props>,
+    prevState: Readonly<State>,
+    snapshot?: any
+  ): void {
+    if (
+      prevProps.router.location.hash === "#@" &&
+      this.props.router.location.hash === ""
+    ) {
+      this.hide();
+    }
+  }
+  public componentWillUnmount(): void {}
+  public render(): React.ReactNode {
+    return (
+      <>
+        {this.activeModal}
+        <div
+          className={`${styles.main} ${this.state.isActive ? styles.active : ""}`}
+          onClick={this.toggle}
+        >
+          <img className={styles.button_image} src={logo} alt="" />
+          <div className={styles.button_container}>
+            <div
+              className={styles.button}
+              onClick={(e: MouseEvent) => {
+                e.stopPropagation();
+                this.hide();
+                this.setState({ activeModalName: "createTradeRecord" });
+              }}
+            >
+              <IconPencilSquare sideLength="20" />
+              <div className={styles.hint_text}>交易紀錄</div>
+            </div>
+            <div
+              className={styles.button}
+              onClick={(e: MouseEvent) => {
+                e.stopPropagation();
+                this.hide();
+                this.setState({ activeModalName: "createCashDividendRecord" });
+              }}
+            >
+              <IconCoin sideLength="20" />
+              <div className={styles.hint_text}>現金股利</div>
+            </div>
+            <div
+              className={styles.button}
+              onClick={(e: MouseEvent) => {
+                e.stopPropagation();
+                this.hide();
+                this.setState({ activeModalName: "createTradePlan" });
+              }}
+            >
+              <IconBrandStrategy sideLength="20" />
+              <div className={styles.hint_text}>買賣計畫</div>
+            </div>
+            <div
+              className={styles.button}
+              onClick={(e: MouseEvent) => {
+                e.stopPropagation();
+                this.hide();
+                this.setState({ activeModalName: "search" });
+              }}
+            >
+              <IconMagnifier sideLength="20" />
+              <div className={styles.hint_text}>搜尋</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.background} onClick={this.hide} />
+      </>
+    );
+  }
+  private get activeModal(): React.ReactElement<typeof Modal> | null {
+    if (this.state.activeModalName === "createTradeRecord") {
+      return (
+        <TradeRecordModal
+          defaultSid={this.props.defaultSid}
+          hideModal={Util.getHideModalCallback(this)}
+        />
+      );
+    } else if (this.state.activeModalName === "createCashDividendRecord") {
+      return (
+        <CashDividendRecordModal
+          defaultSid={this.props.defaultSid}
+          hideModal={Util.getHideModalCallback(this)}
+        />
+      );
+    } else if (this.state.activeModalName === "createTradePlan") {
+      return (
+        <TradePlanModal
+          defaultSid={this.props.defaultSid}
+          hideModal={Util.getHideModalCallback(this)}
+        />
+      );
+    } else if (this.state.activeModalName === "search") {
+      return <StockSearchModal hideModal={Util.getHideModalCallback(this)} />;
+    }
+    return null;
+  }
+  private toggle = (e: MouseEvent): void => {
+    e.stopPropagation();
+    this.setState((state, props) => {
+      if (state.isActive) {
+        setTimeout(() => {
+          if (this.props.router.location.hash === "#@") {
             this.props.router.navigate(-1);
-        }
-        document.body.style.overscrollBehaviorY = "initial";
-        Util.changePWAThemeColor("#fff");
-        this.setState({ isActive: false });
-    };
+          }
+          Util.changePWAThemeColor("#fff");
+          document.body.style.overscrollBehaviorY = "initial";
+        });
+      } else {
+        document.body.style.overscrollBehaviorY = "contain";
+        setTimeout(() => {
+          this.props.router.navigate("#@");
+          Util.changePWAThemeColor("#888");
+        });
+      }
+      return { isActive: !state.isActive };
+    });
+  };
+  private hide = (): void => {
+    if (this.props.router.location.hash === "#@") this.props.router.navigate(-1);
+    document.body.style.overscrollBehaviorY = "initial";
+    Util.changePWAThemeColor("#fff");
+    this.setState({ isActive: false });
+  };
 }
 
 export default withRouter(StretchableButton);
