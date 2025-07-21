f52dc0d543ac3f4d2778f0b3dfad17bdab0a7cb3
Bing-Yang Chen <bryan.chen.429@gmail.com>
2025-07-21 08:32:32 +0800

    Update domain references from trade-smartly.com to taigu.tw across the project
    - Changed domain in Docker Compose configuration for SSL certificates.
    - Updated README to reflect the new domain.
    - Modified Google API client secrets to include the new domain.
    - Adjusted production settings to allow the new domain.
    - Updated frontend package.json, HTML, and sitemap to use the new domain.
    - Changed SSL certificate paths in Nginx configuration files.
    - Updated the certificate acquisition script to use the new domain.
    
    These changes ensure the application is fully transitioned to the new domain.

--- frontend/src/utils/env.tsx
+++ frontend/src/utils/env.tsx
@@ -1,10 +1,10 @@
 import Nav from "./nav";
 
 export default class Env {
-    public static backendUrl: string = Nav.isLocalhost
-        ? `https://${window.location.hostname}/api/`
-        : "https://trade-smartly.com/api/";
-    public static frontendRootPath: string = "/";
-    public static cacheKey: string = "trade-smartly";
-    public static broadcastChannelName: string = "trade-smartly";
+  public static backendUrl: string = Nav.isLocalhost
+    ? `https://${window.location.hostname}/api/`
+    : "https://taigu.tw/api/";
+  public static frontendRootPath: string = "/";
+  public static cacheKey: string = "trade-smartly";
+  public static broadcastChannelName: string = "trade-smartly";
 }
