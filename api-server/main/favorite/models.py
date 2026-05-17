6ea1805fccd08a869d3c3db6cecaec3c2661f671
Bing-Yang Chen <bryan.chen.429@gmail.com>
2026-05-17 13:40:26 +0800

    Add new models for StockMemo and TradePlan; remove deprecated memo models
    - Introduced new Django models: `StockMemo` and `TradePlan`, enhancing the application's functionality for managing stock-related notes and trading plans.
    - Removed the deprecated `memo` models to streamline the codebase and improve maintainability.
    - Updated database schema to reflect these changes, ensuring unique constraints and relationships are properly defined.

---
+++ api-server/main/favorite/models.py
@@ -0,0 +1,21 @@
+from django.db.models import CASCADE, PROTECT, ForeignKey
+
+from main.account.models import User
+from main.core.models import CreateUpdateDateModel
+from main.stock.models import Company
+
+
+class Favorite(CreateUpdateDateModel):
+    owner: User = ForeignKey(
+        User, on_delete=CASCADE, related_name="favorites", db_index=False
+    )  # type: ignore
+    company: Company = ForeignKey(  # type: ignore
+        Company, on_delete=PROTECT, related_name="followers", db_index=False
+    )
+
+    class Meta:
+        db_table = "favorite"
+        unique_together = [["owner", "company"]]
+
+    def __str__(self) -> str:
+        return f"{self.owner.username}_{self.company.pk}"
