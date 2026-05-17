2b03d69467d26bb19f70b82f995011dd26289cc0
Bing-Yang Chen <bryan.chen.429@gmail.com>
2026-05-17 15:51:01 +0800

    move market commands

--- api-server/main/market/management/commands/update_company_list.py
+++ api-server/main/market/management/commands/update_company_list.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from main.stock.services import update_material_facts
+from main.market.services import update_company_list
 
 
 class Command(BaseCommand):
+    """This command is currently not used."""
+
     def handle(self, *args, **options) -> None:  # noqa: ANN002, ANN003
-        update_material_facts()
+        update_company_list()
--- api-server/main/market/management/commands/update_material_facts.py
+++ api-server/main/market/management/commands/update_material_facts.py
@@ -1,10 +1,8 @@
 from django.core.management.base import BaseCommand
 
-from main.stock.services import update_company_list
+from main.market.services import update_material_facts
 
 
 class Command(BaseCommand):
-    """This command is currently not used."""
-
     def handle(self, *args, **options) -> None:  # noqa: ANN002, ANN003
-        update_company_list()
+        update_material_facts()
