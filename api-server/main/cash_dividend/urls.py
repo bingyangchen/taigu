f0c63eec09df93b4efabe0cf8cc119f14b1c505c
Bing-Yang Chen <bryan.chen.429@gmail.com>
2026-05-17 15:44:54 +0800

    Add URL routing for cash dividend and trade record apps; remove deprecated stock URLs
    - Created new `urls.py` files for the `cash_dividend` and `trade_record` apps, defining API endpoints for creating, listing, updating, and deleting records.
    - Removed the outdated `urls.py` for the `stock` app to streamline the project structure and improve maintainability.
    - This update enhances the API routing for cash dividends and trade records, ensuring better organization and access to these features.

---
+++ api-server/main/cash_dividend/urls.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+
+from main.cash_dividend import views
+
+urlpatterns = [
+    re_path(r"^$", views.create_or_list),
+    re_path(r"^(?P<id>\w+)[/]?$", views.update_or_delete),
+]
